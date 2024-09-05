4872553c20076b5c9939c4373e79a4d3015498a6
XxXJayXx <tylix24@gmail.com>
2024-09-05 23:18:38 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// الجزء الثالث من الفلاق
+// qo12991sj}
+
+document.querySelector('.button').addEventListener('click', function() {
+    alert('Keep looking! Maybe the source code has something interesting.');
+});
